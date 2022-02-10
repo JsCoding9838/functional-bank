92d595fe042bf7c86381ca7a95e135b50f92c242
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-02-11 02:42:25 +0800

    deposit and balance section updated

--- js/banking.js
+++ js/banking.js
@@ -1,15 +1,23 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
-    const dipositInput = document.getElementById('deposit-input');
-    const depositAmount = dipositInput.value;
+    const dipositInput      = document.getElementById('deposit-input');
+    const depositAmountText = dipositInput.value;
+    const depositAmount     = parseFloat(depositAmountText);
     // console.log(depositAmount);
 
 
 
     // get current deposit
-    const depositTotal = document.getElementById('deposit-total');
-    const depositTotaltext = depositTotal.innerText;
-    depositTotal.innerText = depositAmount;
-    console.log(depositTotaltext);
+    const depositTotal          = document.getElementById('deposit-total');
+    const depositTotalText      = depositTotal.innerText;
+    const previousDepositTotal  = parseFloat(depositTotalText);
+    depositTotal.innerText      = previousDepositTotal + depositAmount;
+
+    // update balance
+    const balanceTotal          = document.getElementById('balance-total');
+    const balanceTotalText      = balanceTotal.innerText;
+    const previounsBalanceTotal = parseFloat(balanceTotalText);
+
+    balanceTotal.innerText = previounsBalanceTotal + depositAmount;
 
     // clear input field
     dipositInput.value = '';
