0f6dfe9e4e5c7bba8ad1bbeb1c26458c882c82af
Jubayer Hosen <jubayerhosain9838@gmail.com>
2022-02-11 19:56:18 +0800

    withdraw updated

--- js/banking.js
+++ js/banking.js
@@ -2,7 +2,6 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const dipositInput      = document.getElementById('deposit-input');
     const depositAmountText = dipositInput.value;
     const depositAmount     = parseFloat(depositAmountText);
-    // console.log(depositAmount);
 
 
 
@@ -17,8 +16,27 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const balanceTotalText      = balanceTotal.innerText;
     const previounsBalanceTotal = parseFloat(balanceTotalText);
 
-    balanceTotal.innerText = previounsBalanceTotal + depositAmount;
+    balanceTotal.innerText      = previounsBalanceTotal + depositAmount;
 
     // clear input field
     dipositInput.value = '';
+})
+
+
+// handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdrawInput      = document.getElementById('widthdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount     = parseFloat(withdrawAmountText);
+
+    // update withdraw total
+    const withdrawTotal             = document.getElementById('withdraw-total');
+    const previousWithdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal     = parseFloat(previousWithdrawTotalText);
+    withdrawTotal.innerText         = previousWithdrawTotal + withdrawAmount;
+
+    // clear withdraw input field
+    withdrawInput = '';
+    
+
 })
